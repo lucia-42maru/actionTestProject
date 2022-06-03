59b0a0cf2b0885aaf46068398637c256c5ca30c2
dlckdgk0902@gmail.com <dlckdgk0902@gmail.com>
2022-06-03 16:41:31 +0900

    220603 git action test - sphinx.yml 수정

--- conf.py
+++ conf.py
@@ -147,8 +147,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'test_project', 'Test_project Documentation',
-     [author], 1)
+    (master_doc, 'test_project', 'Test_project Documentation', [author], 1)
 ]
 
 
