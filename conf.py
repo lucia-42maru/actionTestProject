be05d211c8003e63658a2551ec368156f02b7227
dlckdgk0902@gmail.com <dlckdgk0902@gmail.com>
2022-06-03 17:29:40 +0900

    220603 git action test

--- conf.py
+++ conf.py
@@ -14,7 +14,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../src'))
+sys.path.insert(0, os.path.abspath('src'))
 
 
 # -- Project information -----------------------------------------------------
@@ -56,8 +56,8 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-source_suffix = ['.rst', '.md']
-# source_suffix = '.rst'
+# source_suffix = ['.rst', '.md']
+source_suffix = '.rst'
 
 # The master toctree document.
 master_doc = 'index'
